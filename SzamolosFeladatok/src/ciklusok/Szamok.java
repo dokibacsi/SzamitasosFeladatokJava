c45f597d2491fc97edbe612eba38e195c3c69052
dokibacsi <kvalcsi02@gmail.com>
2023-03-02 16:03:53 +0100

    2-3-4 szamos feladat

--- SzamolosFeladatok/src/ciklusok/Szamok.java
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -1,6 +1,12 @@
 package ciklusok;
 public class Szamok {
     public static void main(String[] args) {
-        
+        for (int z = 2; z <=4; z++) {
+            for (int j = 2; j <= 4; j++) {
+                for (int i = 2; i <= 4; i++) {
+                    System.out.print(z+""+j+""+i+"\n");
+                }
+            }
+        }
     }
 }
